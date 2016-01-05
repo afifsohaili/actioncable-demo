fb107471bdbd47d281c8f394e392a9489c64458b
afifsohaili <afifnajib@gmail.com>
2016-01-05 17:12:19 +0800

    Add keypress control for easier navigation with mobile phone

---
+++ app/assets/javascripts/keypress_control.js
@@ -0,0 +1,6 @@
+$(function () {
+  $(document).on("keypress", function(e) {
+    if (e.keyCode === 97) { $("#prev").trigger("click") }
+    else if (e.keyCode === 108) { $("#next").trigger("click") }
+  });
+});
