140badfe98d8449ac158ee76f34f9b51888d569c
afifsohaili <afifnajib@gmail.com>
2016-01-05 17:03:46 +0800

    Booster splash page

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,7 +1,11 @@
 class DecksController < ApplicationController
+  layout "slide_deck"
+
   def show
     render "show", locals: { deck: deck }
   end
+
+  private
 
   def deck
     @deck ||= DeckPresenter.new(Deck.find_by(slug: params[:id]))
